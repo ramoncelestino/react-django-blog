7820bfb50bb9bdeaa0dce1d95a3b460c3074fae6
Ramon Celestino <ramoncelesteramos@gmail.com>
2021-04-30 22:26:13 -0300

    User details

--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -2,8 +2,12 @@ from rest_framework import serializers
 from .models import Person
 from .models import Address
 from .models import Post
+from django.contrib.auth.models import User
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
@@ -18,6 +22,7 @@ class PersonSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'email', 'phone', 'address') 
 
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = Post 
         fields = '__all__'
